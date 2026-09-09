7ab5ad058a4b7429e9a67c8d767b5a73741b5703
ssdhameliya <ssdhameliya@gmail.com>
2026-09-09 22:37:59 +0530

    Restore Phase 6/7 PWA parity - config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.JASVI_PWA_CONFIG = Object.freeze({
   appName: 'Jasvi Industries',
   environment: 'UAT',
-  version: '1.0.1-uat.3',
+  version: '1.1.0-uat-phase7-parity',
   apiBaseUrl: 'https://api-uat.jasviindustries.in',
   publicUrl: 'https://mobile-uat.jasviindustries.in',
   prodPublicUrl: 'https://mobile.jasviindustries.in',
   prodApiBaseUrl: 'https://api.jasviindustries.in',
   enableProdSwitch: false,
-  pageSize: 25,
+  pageSize: 50,
   requestTimeoutMs: 30000
 });
