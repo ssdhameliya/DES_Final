548ade3aa03fe24444e69fd1b0ef66ec007b80fe
ssdhameliya <ssdhameliya@gmail.com>
2026-09-09 19:47:12 +0530

    Add UAT PWA configuration

---
+++ config.js
@@ -0,0 +1,12 @@
+window.JASVI_PWA_CONFIG = Object.freeze({
+  appName: 'Jasvi Industries',
+  environment: 'UAT',
+  version: '1.0.0-uat.2',
+  apiBaseUrl: 'https://api-uat.jasviindustries.in',
+  publicUrl: 'https://mobile-uat.jasviindustries.in',
+  prodPublicUrl: 'https://mobile.jasviindustries.in',
+  prodApiBaseUrl: 'https://api.jasviindustries.in',
+  enableProdSwitch: false,
+  pageSize: 25,
+  requestTimeoutMs: 30000
+});
